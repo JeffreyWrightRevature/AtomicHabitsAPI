74cd28e79b17e746858e5128457527ebb5ba4807
Jeffrey Wright <jeffrey.wright@revature.net>
2021-10-26 19:43:43 -0400

    Started work on the quotes API

---
+++ AtomicHabitsAPI/DatabaseLayer/QuotesContext.cs
@@ -0,0 +1,18 @@
+﻿using AtomicHabitsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtomicHabitsAPI.DatabaseLayer
+{
+	public class QuotesContext : DbContext
+	{
+		public QuotesContext(DbContextOptions<QuotesContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Quote> Quotes { get; set; }
+	}
+}
---
+++ AtomicHabitsAPI/Models/Quote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtomicHabitsAPI.Models
+{
+	public class Quote
+	{
+		public int Id { get; set; }
+		public string Text { get; set; }
+		public int Page { get; set; }
+		public string Category { get; set; }
+	}
+}
